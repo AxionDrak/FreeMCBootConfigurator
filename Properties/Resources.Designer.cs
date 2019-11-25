0e30b5c32bbda4011e61fed563342229c7db6289
Laete Meireles <laetemn@gmail.com>
2019-11-25 13:47:36 -0300

    Properties

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace FreeMCBootConfigurator.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap paypal {
+            get {
+                object obj = ResourceManager.GetObject("paypal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap question {
             get {
                 object obj = ResourceManager.GetObject("question", resourceCulture);
